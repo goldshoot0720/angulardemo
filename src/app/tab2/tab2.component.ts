69080cf433d4172bf7d14d4d22223867442c9d3f
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-11 00:10:11 +0800

    angulardemo tab8

--- src/app/tab2/tab2.component.ts
+++ src/app/tab2/tab2.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import {TablemyComponent} from "../tablemy/tablemy.component";
 
 @Component({
   selector: 'app-tab2',
-  imports: [],
+    imports: [
+        TablemyComponent
+    ],
   templateUrl: './tab2.component.html',
   styleUrl: './tab2.component.css'
 })
 export class Tab2Component {
-
+  columns = [
+    {
+      column1: 'Mirtazapine O.D 30mg/ tab (Sinmaron)',
+      column2: '25天(相當於3週又4天)',
+      column3: '天晟/譚詠康(身心科)',
+      column4: '無',
+    },
+  ];
+  columns2 = [
+    {
+      column1: '天晟/譚詠康(身心科)',
+      column2: '05/26(一)~06/05(四)',
+      column3: '06/23(一)~07/03(四',
+      column4: '07/31(四)下午',
+    },
+    {
+      column1: '天晟/黃信凱(心臟內科)',
+      column2: '',
+      column3: '05/27(二)~06/06(五)',
+      column4: '07/04(五)下午[預定改成07/11(五)下午]',
+    },
+    {
+      column1: '范光迪/陳建鴻',
+      column2: '[預定改成06/30(一)上午][預定改成07/03(三)上午]',
+      column3: '[預定改成07/10(三)上午][預定改成07/11(四)上午]',
+      column4: '06/27(五)上午[預定改成07/12(五)上午]',
+    },
+    {
+      column1: '晶鑽牙醫',
+      column2: '',
+      column3: '',
+      column4: '08/00(無)下午',
+    },
+  ];
 }
--- src/app/tab8/tab8.component.ts
+++ src/app/tab8/tab8.component.ts
@@ -1,11 +1,296 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {TablemyComponent} from '../tablemy/tablemy.component';
 
 @Component({
   selector: 'app-tab8',
-  imports: [],
+  imports: [
+    TablemyComponent
+  ],
   templateUrl: './tab8.component.html',
   styleUrl: './tab8.component.css'
 })
 export class Tab8Component {
+  columns = [
+    {
+      column1: '合作金庫',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '合作金庫',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '台北富邦',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '台北富邦',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '國泰世華',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '國泰世華',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '兆豐銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '兆豐銀行',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '王道銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '王道銀行',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },    {
+      column1: '新光銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '新光銀行',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '中華郵政',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '中華郵政',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '玉山銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '玉山銀行',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '台新銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '台新銀行',
+      column2: '定存',
+      column3: '3333',
+      column4: '',
+    },
+    {
+      column1: '中國信託',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '中國信託',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '鋒兄銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '鋒兄銀行',
+      column2: '定存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '鋒兄銀行',
+      column2: '活存',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '鋒兄銀行',
+      column2: '定存利息',
+      column3: '',
+      column4: '',
+    },
+  ];
+  columns2 = [
+    {
+      column1: '街口支付',
+      column2: '',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '悠遊付',
+      column2: '',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: 'Samsung Wallet',
+      column2: '548',
+      column3: '悠遊卡',
+      column4: '',
+    },
+    {
+      column1: '小米手環9NFC',
+      column2: '',
+      column3: 'SuperCard超級悠遊卡',
+      column4: '冰川銀',
+    },
+    {
+      column1: '小米手環9NFC',
+      column2: '',
+      column3: 'SuperCard超級悠遊卡',
+      column4: '午夜黑',
+    },
+    {
+      column1: '全聯',
+      column2: '19',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '家樂福',
+      column2: '23',
+      column3: '',
+      column4: '',
+    },
+  ];
 
+  columns3 = [
+    {
+      column1: '台灣行動支付',
+      column2: '♾️',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '合作金庫',
+      column2: '6',
+      column3: '6',
+      column4: '',
+    },
+    {
+      column1: '台北富邦',
+      column2: '5',
+      column3: '5',
+      column4: '',
+    },
+    {
+      column1: '國泰世華',
+      column2: '',
+      column3: '',
+      column4: '資產50萬元(含)以上，「臺幣跨轉5次免手續費」優惠券',
+    },
+    {
+      column1: '兆豐銀行',
+      column2: '',
+      column3: '',
+      column4: '數位存款 10/10',
+    },
+    {
+      column1: '王道銀行',
+      column2: '5',
+      column3: '',
+      column4: '2萬元（含）以上，跨行提款3次',
+    },    {
+      column1: '新光銀行',
+      column2: '3',
+      column3: '3',
+      column4: '',
+    },
+    {
+      column1: '中華郵政',
+      column2: '',
+      column3: '',
+      column4: '',
+    },
+    {
+      column1: '玉山銀行',
+      column2: '3',
+      column3: '3',
+      column4: '',
+    },
+    {
+      column1: '台新銀行',
+      column2: '5',
+      column3: '5',
+      column4: 'Richart',
+    },
+    {
+      column1: '中國信託',
+      column2: '1',
+      column3: '1(他行卡，事後)',
+      column4: '中信交易優惠券',
+    },
+  ];
+
+  ngOnInit(): void {
+    let oddSum = 0;
+    let evenSum = 0;
+
+    this.columns.forEach((item, index) => {
+      const value = parseFloat(item.column3) || 0;
+      if ((index + 1) % 2 === 1) {
+        oddSum += value;
+      } else {
+        evenSum += value;
+      }
+    });
+
+    this.columns = this.columns.map(item => {
+      if (item.column1 === '鋒兄銀行' && item.column2 === '活存') {
+        return { ...item, column3: oddSum.toString() };
+      } else if (item.column1 === '鋒兄銀行' && item.column2 === '定存') {
+        return { ...item, column3: evenSum.toString() };
+      } else if (item.column1 === '鋒兄銀行' && item.column2 === '定存利息') {
+        return { ...item, column3: (Math.floor(evenSum*1.715/100)).toString() };
+      } else {
+        return item;
+      }
+    });
+  }
 }
