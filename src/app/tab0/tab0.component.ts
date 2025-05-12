9a8b0f089cc4de87d632e59c9ab04f0cc324ca65
goldshoot0720 <goldshoot0720@gmail.com>
2025-05-13 02:18:00 +0800

    angulardemo container

--- src/app/tab0/tab0.component.ts
+++ src/app/tab0/tab0.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-tab0',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './tab0.component.html',
   styleUrl: './tab0.component.css'
 })
